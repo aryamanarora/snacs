4d31d90a1bb5116cec982b0d3c2d899c05034282
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-03-19 21:46:06 -0400

    basic finetuning of BERT on en-lp

---
+++ load_data.py
@@ -0,0 +1,59 @@
+import conllu
+from transformers import AutoTokenizer
+import glob
+from tqdm import tqdm
+
+conllulex = ['id', 'form', 'lemma', 'upos', 'xpos', 'feats', 'head','deprel', 'deps',
+             'misc', 'smwe', 'lexcat', 'lexlemma', 'ss', 'ss2', 'wmwe', 'wcat', 'wlemma', 'lextag']
+
+def tokenize_and_align(file: str, tokenizer: AutoTokenizer, add_space: bool=True, hide_non_adp: bool=True):
+    print(file)
+    res = []
+    with open(file, 'r') as fin:
+
+        failed = 0
+        for sent in tqdm(conllu.parse_incr(fin, fields=conllulex)):
+            text = sent.metadata['text']
+
+            tokens = []
+            mask = []
+            labels = []
+            work = True
+
+            for i, token in enumerate(sent):
+                if not isinstance(token['id'], int): continue
+
+                if hide_non_adp:
+                    if '-P-' not in token['lextag']:
+                        token['lextag'] = 'O'
+
+                tok = None
+                if i == 0 or not add_space: tok = tokenizer.encode(token['form'])
+                else: tok = tokenizer.encode(' ' + token['form'])
+
+                labels.append(token['lextag'])
+                labels.extend(['None' for _ in range(len(tok) - 1)])
+                mask.append(1)
+                mask.extend([0 for _ in range(len(tok) - 1)])
+                tokens.extend(tok)
+            
+            if work:
+                res.append([tokens, mask, labels])
+
+    print(res[0])
+    if failed != 0:
+        print(f"Failed to parse {failed} sentences, did {len(res)}.")
+    else:
+        print(f"Done: {len(res)} sentences.")
+
+    return res
+
+
+def main():
+    for file in glob.glob("data/*.conllulex"):
+        print(file)
+        tokenizer = AutoTokenizer.from_pretrained("gpt2")
+        tokenize_and_align(file, tokenizer)
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,92 @@
+from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer, AutoTokenizer, DataCollatorForTokenClassification
+from load_data import tokenize_and_align
+import numpy as np
+import evaluate
+
+seqeval = evaluate.load("seqeval")
+
+def load_data(file: str, tokenizer: AutoTokenizer):
+    res = tokenize_and_align(file, tokenizer)
+
+    # make label-id mapping
+    label_to_id = {"None": -100}
+    id_to_label = {-100: "None"}
+    for sent, mask, label in res:
+        for i in range(len(sent)):
+            if mask[i]:
+                if label[i] not in label_to_id:
+                    id = len(label_to_id)
+                    label_to_id[label[i]] = id
+                    id_to_label[id] = label[i]
+
+    res2 = []
+
+    # add sos and eos, convert labels to ids
+    sos_eos = tokenizer("")['input_ids']
+    for sent, mask, label in res:
+        if len(sos_eos) == 2:
+            sent = [sos_eos[0]] + sent + [sos_eos[1]]
+            mask = [0] + mask + [0]
+            label = ["None"] + label + ["None"]
+        label = [label_to_id[x] for x in label]
+        res2.append({
+            'input_ids': sent,
+            'mask': mask,
+            'labels': label
+        })
+    
+    print(f"{len(label_to_id)} labels.")
+    return res2, label_to_id, id_to_label
+
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    results = seqeval.compute(predictions=predictions, references=labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }
+
+def train(model_name: str, file: str):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    data, label_to_id, id_to_label = load_data(file, tokenizer)
+
+    model = AutoModelForTokenClassification.from_pretrained(
+        model_name, num_labels=len(label_to_id), id2label=id_to_label, label2id=label_to_id
+    )
+
+    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+    
+    training_args = TrainingArguments(
+        output_dir="models",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=10,
+        weight_decay=0.01,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+        push_to_hub=True,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=data[20:],
+        eval_dataset=data[:20],
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+def main():
+    train("bert-base-uncased", "data/en-lp.conllulex")
+
+if __name__ == "__main__":
+    main()
