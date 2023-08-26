33d699b49efb0ab85eeb5cfec108892d499139b1
WesScivetti <wesleyscivetti@my.unt.edu>
2023-08-26 14:20:23 -0400

    Update train.py

--- train.py
+++ train.py
@@ -48,6 +48,8 @@ def load_data(file: str, tokenizer: AutoTokenizer, id_to_label = None, label_to_
     
     print(f"{len(label_to_id)} labels.")
     random.shuffle(res2)
+
+    print(len(res2[0]["input_ids"]), len(res2[0]["labels"]), file=sys.stderr)
     
     return res2, label_to_id, id_to_label
 
