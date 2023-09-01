828ff2f63f5ff5e9ff72b14c0b6b051d5fc1ad86
WesScivetti <wesleyscivetti@my.unt.edu>
2023-09-01 15:58:47 -0400

    Update train.py

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def load_data(file: str, tokenizer: AutoTokenizer, id_to_label = None, label_to_
         for tag_type in ["lt", "ss", "ss2"]:
             all_tags = list(set(list(old_freqs[tag_type].keys()) + list(new_freqs[tag_type].keys())))
             if "B-p.Cost-Extent" in all_tags:
-                print("HOW SO")
+                print(all_tags)
             for tag in all_tags:
                 freqs[tag_type][tag] = old_freqs[tag_type][tag] + new_freqs[tag_type][tag]
 
